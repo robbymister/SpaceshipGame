3cffe05b083ff0740ff775e10bfac72e99415ef6
Robert E <44422175+robbymister@users.noreply.github.com>
2019-01-09 01:14:48 -0500

    Add files via upload

---
+++ Ball.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+    public float difficultyMultiplier = 1.5f;
+
+    public float minXspeed = 0.8f;
+    public float maxXspeed = 1.2f;
+    public float minYspeed = 0.8f;
+    public float maxYspeed = 1.2f;
+    private Rigidbody2D ballBody;
+	// Use this for initialization
+	void Start () {
+        ballBody = GetComponent<Rigidbody2D>();
+        ballBody.velocity = new Vector2(Random.Range(minXspeed,maxXspeed) * (Random.value > 0.5f ? -1 : 1), Random.Range(minYspeed, maxYspeed) * (Random.value > 0.5f ? -1:1));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Limit")
+        {
+            GetComponent<AudioSource>().Play();
+            if (other.transform.position.y > transform.position.y && ballBody.velocity.y > 0) // Top-limit
+            {
+                ballBody.velocity = new Vector2(ballBody.velocity.x, -ballBody.velocity.y);
+            } if (other.transform.position.y < transform.position.y && ballBody.velocity.y < 0) // Bot limit
+            {
+                ballBody.velocity = new Vector2(ballBody.velocity.x, -ballBody.velocity.y);
+            } 
+        }
+        else if (other.tag == "Paddle")
+        {
+            GetComponent<AudioSource>().Play();
+            if (other.transform.position.x > transform.position.x && ballBody.velocity.x > 0) // Moving right
+            {
+                ballBody.velocity = new Vector2(-ballBody.velocity.x * difficultyMultiplier, ballBody.velocity.y * difficultyMultiplier);
+            }
+            if (other.transform.position.x < transform.position.x && ballBody.velocity.x < 0) // Moving left
+            {
+                ballBody.velocity = new Vector2(-ballBody.velocity.x * difficultyMultiplier, -ballBody.velocity.y * difficultyMultiplier);
+            }
+        }
+    }
+}
---
+++ GameController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject ballPrefab;
+    public Text score1Text;
+    public Text score2Text;
+    public float scoreCoords = 3.4f;
+
+    private Ball currentBall;
+    private int score1 = 0;
+    private int score2 = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+        spawnBall();
+    }
+
+    void spawnBall()
+    {
+        GameObject ballInstance = Instantiate(ballPrefab, transform);
+        currentBall = ballInstance.GetComponent<Ball>();
+        currentBall.transform.position = Vector3.zero;
+        score1Text.text = score1.ToString();
+        score2Text.text = score2.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentBall != null)
+        {
+            if (currentBall.transform.position.x > scoreCoords)
+            {
+                score1++;
+                Destroy(currentBall.gameObject);
+                spawnBall();
+            }
+            if (currentBall.transform.position.x < -scoreCoords)
+            {
+                score2++;
+                Destroy(currentBall.gameObject);
+                spawnBall();
+            }
+        }
+    }
+}
